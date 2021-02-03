060d71a7f2c4f1a9b23b79852a6086c7ab4d44ae
Yunyun <celeph4is@gmail.com>
2021-02-03 15:31:51 -0800

    add typing for 'entries' field of Patient objects

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,47 @@
-export interface Entry {
-
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>; // array of Diagnosis objects, specifically the 'code field'
 }
+
+enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3
+}
+  
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck'; 
+  healthCheckRating: HealthCheckRating;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital'
+  discharge: Discharge;
+}
+
+export type Entry =
+| HospitalEntry
+| OccupationalHealthcareEntry
+| HealthCheckEntry;
 
 export interface Diagnosis {
   code: string;
